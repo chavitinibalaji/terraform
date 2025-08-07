70fe24dee6a771aba0791f50a468181cd66abfec
chavitinibalaji <balajichavitini@gmail.com>
2025-08-07 22:56:27 +0530

    day-4

--- day-4/main.tf
+++ day-4/main.tf
@@ -1,14 +1,26 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+# VPC Resource
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# EC2 Instance Resource
 resource "aws_instance" "name" {
-    ami = "ami-084a7d336e816906b"
-    instance_type = "t3.micro"
-    tags = {
+  ami           = "ami-084a7d336e816906b"
+  instance_type = "t2.micro"
+
+  tags = {
     Name = "MyInstance"
   }
 }
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      name ="balu-vpc-1"
-    }
-  
-}
+
