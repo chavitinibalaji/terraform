604a8eddfb77712fec6cd7e8eec57964cd0abaf3
chavitinibalaji <balajichavitini@gmail.com>
2025-09-15 13:26:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"
-}
-
-# VPC Resource
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "MyVPC-1"
-  }
+  
 }
 
 # EC2 Instance Resource
 resource "aws_instance" "name" {
-  ami           = "ami-084a7d336e816906b"
+  ami           = "ami-0b09ffb6d8b58ca91"
   instance_type = "t2.micro"
 
   tags = {
